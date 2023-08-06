982515aa08cd2154b1df7e41f9d823a31cd9467c
vincent <100250184+vguzman812@users.noreply.github.com>
2023-08-06 17:57:30 -0400

    add geolocation file and function using ip-api.

--- assets/js/cloudflare.js
+++ assets/js/cloudflare.js
@@ -1,55 +1,67 @@
-// Define the API endpoint
-const ipUrl = "https://1.1.1.1/cdn-cgi/trace";
-
-const url = "https://www.cloudflare.com/cdn-cgi/trace";
-
 /*
 Initial Return Format
 
-  fl=Cloudflare WebServer Instance
-  h=WebServer Hostname
-  ip=IP Address of client
-  ts=Epoch Time in seconds.millis (Similar to `date +%s.%3N` in bash)
-  visit_scheme=https or http
-  uag=User Agent
-  colo=IATA location identifier
-  sliver=Whether the request is splitted
-  http=HTTP Version
-  loc=Country Code
-  tls=TLS or SSL Version
-  sni=Whether SNI encrypted or plaintext
-  warp=Whether client over Cloudflares Wireguard VPN
-  gateway=Whether client over Cloudflare Gateway
-  rbi=Whether client over Cloudflares Remote Browser Isolation
-  kex=Key exchange method for TLS
+    fl=Cloudflare WebServer Instance
+    h=WebServer Hostname
+    ip=IP Address of client
+    ts=Epoch Time in seconds.millis (Similar to `date +%s.%3N` in bash)
+    visit_scheme=https or http
+    uag=User Agent
+    colo=IATA location identifier
+    sliver=Whether the request is splitted
+    http=HTTP Version
+    loc=Country Code
+    tls=TLS or SSL Version
+    sni=Whether SNI encrypted or plaintext
+    warp=Whether client over Cloudflares Wireguard VPN
+    gateway=Whether client over Cloudflare Gateway
+    rbi=Whether client over Cloudflares Remote Browser Isolation
+    kex=Key exchange method for TLS
+
+    fl=17f454
+    h=www.cloudflare.com
+    ip=2601:58a:8a83:f8f0:714a:d889:586:be60
+    ts=1691357792.238
+    visit_scheme=https
+    uag=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36
+    colo=MIA
+    sliver=none
+    http=http/3
+    loc=US
+    tls=TLSv1.3
+    sni=plaintext
+    warp=off
+    gateway=off
+    rbi=off
+    kex=X25519
 
 */
 
-// Use the fetch method to get data from the API
-fetch(url)
-    .then((response) => {
+// export and get user IP address
+export async function getUserIpAddress() {
+    // api endpoints. both the same, but both provided for comprehensiveness.
+    // const ipUrl = "https://1.1.1.1/cdn-cgi/trace";
+
+    const url = "https://www.cloudflare.com/cdn-cgi/trace";
+    try {
+        const response = await fetch(url);
+
         // Check if the request was successful
         if (!response.ok) {
-            throw new Error(`Network response was not ok: ${response}`);
+            throw new Error(`Network response was not ok: ${response.statusText}`);
         }
-        // Parse the data from the response. Initially not in JSON format.
-        return response.text();
-    })
-    .then((data) => {
-        // Handle the parsed data
-        // data to array.
-        data = data
-            .trim()
-            .split("\n")
-            .map((e) => e.split("="));
-        // data from array to object
-        jsonified = Object.fromEntries(data);
-        console.log(jsonified.ip);
-    })
-    .catch((error) => {
-        // Handle any errors that occurred during the fetch
-        console.error(
-            "There was a problem with the fetch operation:",
-            error.message
-        );
-    });
+
+        const dataText = await response.text();
+        const dataArray = dataText.trim().split("\n").map((e) => e.split("="));
+        const dataJson = Object.fromEntries(dataArray);
+        return dataJson.ip; // Log only the IP address
+    }
+    catch (error) {
+        console.error('There was a problem with the fetch operation:', error.message);
+        throw error
+    }
+}
+
+
+
+
---
+++ assets/js/geolocation.js
@@ -0,0 +1,79 @@
+/* Sample Request and Response
+
+https://ip-api.com/docs/api:json
+
+Request: http://ip-api.com/json/24.48.0.1
+
+Response:
+
+{
+    "query": "IP used for the query"
+    "status": "success or fail",
+    "message": "included only when status is fail. Can be one of the following: private range, reserved range, invalid query.",
+    "continent": "Continent name",
+    "continentCode": "Two-letter continent code",
+    "country": "Country name",
+    "countryCode": "Two-letter country code ISO 3166-1 alpha-2",
+    "region": "Region/state short code (FIPS or ISO)",
+    "regionName": "Region/state",
+    "city": "City",
+    "district": "District (subdivision of city)",
+    "zip": "Zip code",
+    "lat": "Latitude",
+    "lon": "Longitude",
+    "timezone": "Timezone (tz)",
+    "offset": "Timezone UTC DST offset in seconds",
+    "currency": "National currency",
+    "isp": "ISP name",
+    "org": "Organization name",
+    "as": "AS number and organization, separated by space (RIR). Empty for IP blocks not being announced in BGP tables.",
+    "asname": "AS name (RIR). Empty for IP blocks not being announced in BGP tables.",
+    "reverse": "Reverse DNS of the IP (can delay response)",
+    "mobile": "Mobile (cellular) connection",
+    "proxy": "Proxy, VPN or Tor exit address",
+    "hosting": "Hosting, colocated or data center",
+}
+
+{
+  "query": "24.48.0.1",
+  "status": "success",
+  "country": "Canada",
+  "countryCode": "CA",
+  "region": "QC",
+  "regionName": "Quebec",
+  "city": "Montreal",
+  "zip": "H2Y",
+  "lat": 45.504,
+  "lon": -73.552,
+  "timezone": "America/Toronto",
+  "isp": "Le Groupe Videotron Ltee",
+  "org": "Videotron Ltee",
+  "as": "AS5769 Videotron Telecom Ltee"
+}
+
+*/
+
+// import ip address from cloudflare.js
+import { getUserIpAddress } from "cloudflare.js";
+
+// export and get latitude and longitude from api
+export async function getLatLon() {
+    const url = "http://ip-api.com/json/"
+    try {
+        const ip = await getUserIpAddress();
+        const response = await fetch(url + ip);
+        // if bad response, throw error
+        if (!response.ok) {
+            throw new Error(`Failed to fetch lat/lon. Status: ${response.statusText}`)
+        }
+        const data = await response.json()
+        return {
+            latitude: data.lat,
+            longitude: data.lon
+        }
+    }
+    catch(error) {
+        console.error('Error: ', error)
+        throw error;
+    }
+}
